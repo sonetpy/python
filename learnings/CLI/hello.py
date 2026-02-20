92bcecf9a3b61de68360cc7647e056ba50cbe2a4
Kumar Abhinav <kumar.abhinav2209@gmail.com>
2026-02-20 09:14:19 +0530

    cli test example

---
+++ learnings/CLI/hello.py
@@ -0,0 +1,16 @@
+# mycli.py
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Simple Greeting CLI")
+
+parser.add_argument("--fname", required=True, help="First Name")
+parser.add_argument("--lname", required=True, help="Last Name")
+
+args = parser.parse_args()
+
+# Capitalize first letter
+first = args.fname.capitalize()
+last = args.lname.capitalize()
+
+print(f"hello {first} {last}!")
