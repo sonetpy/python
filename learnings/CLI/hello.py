610a7cdb27837b258494a086dbe6bc2d5e8c073a
Kumar Abhinav <kumar.abhinav2209@gmail.com>
2026-02-20 09:21:24 +0530

    test cli

--- learnings/CLI/hello.py
+++ learnings/CLI/hello.py
@@ -2,16 +2,19 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description="Simple Greeting CLI")
+parser = argparse.ArgumentParser(description="Greeting CLI")
 
 parser.add_argument("--fname", required=True, help="First Name")
+parser.add_argument("--mname", help="Middle Name")
 parser.add_argument("--lname", required=True, help="Last Name")
 
 args = parser.parse_args()
 
-# Capitalize first letter
 first = args.fname.capitalize()
 last = args.lname.capitalize()
 
-print(f"hello {first} {last}!")
-# cli command: python mycli.py --fname john --lname doe
+if args.mname:
+    middle = args.mname.capitalize()
+    print(f"hello {first} {middle} {last}!")
+else:
+    print(f"hello {first} {last}!")
