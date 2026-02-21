c7bca80a235f87ce3dfb813445947c3e4adc0a88
Kumar Abhinav <kumar.abhinav2209@gmail.com>
2026-02-21 22:26:39 +0530

    cli

--- learnings/CLI/hello.py
+++ learnings/CLI/hello.py
@@ -2,19 +2,22 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description="Greeting CLI")
+def main():
+    parser = argparse.ArgumentParser(description="Greeting CLI")
 
-parser.add_argument("--fname", required=True, help="First Name")
-parser.add_argument("--mname", help="Middle Name")
-parser.add_argument("--lname", required=True, help="Last Name")
+    parser.add_argument("--fname", required=True, help="First Name")
+    parser.add_argument("--mname", help="Middle Name")
+    parser.add_argument("--lname", required=True, help="Last Name")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-first = args.fname.capitalize()
-last = args.lname.capitalize()
+    first = args.fname.capitalize()
+    last = args.lname.capitalize()
 
-if args.mname:
-    middle = args.mname.capitalize()
-    print(f"hello {first} {middle} {last}!")
-else:
-    print(f"hello {first} {last}!")
+    if args.mname:
+        middle = args.mname.capitalize()
+        print(f"hello {first} {middle} {last}!")
+    else:
+        print(f"hello {first} {last}!")
+if __name__ == "__main__":
+    main()
